4d1315f584ba3cb3699ec7bbe8e5410c023bb6a1
João Alves <jneto2727@gmail.com>
2021-03-10 12:43:52 -0300

    refactor: use manual link on example app

--- example/android/app/src/main/java/com/rnbootsplashexample/MainApplication.java
+++ example/android/app/src/main/java/com/rnbootsplashexample/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+
+import com.zoontek.rnbootsplash.RNBootSplashPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          packages.add(new RNBootSplashPackage());
           return packages;
         }
 
